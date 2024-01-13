e92a046639f908b873fbd129b714b58fd795b68f
bryancio506 <bryan.romero.chinchilla@gmail.com>
2024-01-12 18:09:52 -0600

    added Google Analytics

---
+++ components/Common/MesureMetrics.jsx
@@ -0,0 +1,11 @@
+import ReactGA from 'react-ga';
+
+const MesureMetrics = () => {
+    // replace this with your google analytics id
+    const GA_ID = 'G-0QD8PNG4RM'; 
+
+    ReactGA.initialize(GA_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+};
+
+export default MesureMetrics;
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import '../styles/globals.css'
+import MesureMetrics from '../components/Common/MesureMetrics'
 
+import ReactGA from 'react-ga';
 import Layout from './layout'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { useEffect } from 'react';
 
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    MesureMetrics();
+  }, []);
+
+
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
