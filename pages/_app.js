de890ff4036e63245f8a480ac5c33527a8886172
bryancio506 <bryan.romero.chinchilla@gmail.com>
2024-02-07 02:20:49 -0600

    Google Analitics test and updated font size and

--- pages/_app.js
+++ pages/_app.js
@@ -11,9 +11,20 @@ const queryClient = new QueryClient()
 export default function App({ Component, pageProps }) {
 
   useEffect(() => {
-    MesureMetrics();
-  }, []);
+    ReactGA.initialize('G-0QD8PNG4RM');
+    // Track initial pageview
+    ReactGA.pageview(window.location.pathname + window.location.search);
 
+    // Track pageview on route change
+    const handleRouteChange = (url) => {
+      ReactGA.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
